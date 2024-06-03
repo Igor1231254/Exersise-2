d18c0b1347e289f86c7882886c6c937fb6f648cb
Igor1231254 <90178024+Igor1231254@users.noreply.github.com>
2024-06-03 13:07:50 +0300

    Завдання 4.py

--- Завдання 4.py
+++ Завдання 4.py
@@ -5,13 +5,13 @@ str2 = "World"
 str3 = "Python"
 str4 = "Quant"
 
-expression1 = (a > b) or (str1 == "Apple")                 # False or True -> True
-expression2 = (str3 == "Python") or (str4 == "Coding")     # True or False -> True
+expression1 = (a > b) or (str1 == "Apple")               
+expression2 = (str3 == "Python") or (str4 == "Coding")  
 
-expression3 = (a > b) or (str2 == "Universe")              # False or False -> False
-expression4 = (str1 == "Hi") or (str4 == "Coding")         # False or False -> False
+expression3 = (a > b) or (str2 == "Universe")             
+expression4 = (str1 == "Hi") or (str4 == "Coding")      
 
-print("expression1:", expression1)  # Повинно вивести: True
-print("expression2:", expression2)  # Повинно вивести: True
-print("expression3:", expression3)  # Повинно вивести: False
-print("expression4:", expression4)  # Повинно вивести: False
+print("expression1:", expression1)
+print("expression2:", expression2)
+print("expression3:", expression3) 
+print("expression4:", expression4)  
