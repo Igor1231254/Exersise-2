24016bb9f0529ebba7b34715a3c6e828af7f9d8c
Igor1231254 <90178024+Igor1231254@users.noreply.github.com>
2024-06-04 18:03:59 +0300

    Delete Завдання 14.2.py

--- Завдання 14.2.py
+++
@@ -1,8 +0,0 @@
-numbers = []
-for i in range(6):
-    number = float(input(f"Введіть число {i + 1}: "))
-    numbers.append(number)
-
-average = sum(numbers) / len(numbers)
-
-print(f"Середнє арифметичне введених чисел: {average}")
